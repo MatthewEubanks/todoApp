a2f9d10279269262a076f2ec8c9ba474c4c014ac
Matt Eubanks <spc.eubanks.matthew@gmail.com>
2019-02-23 12:32:17 -0700

    final

--- todos_api/Public/app.js
+++ todos_api/Public/app.js
@@ -6,6 +6,15 @@ $(document).ready(function() {
       createTodo();
     }
   });
+
+  $('.list').on('click', 'li', function() {
+    updateTodo($(this));
+  });
+
+  $('.list').on('click', 'span', function(e) {
+    e.stopPropagation();
+    removeTodo($(this).parent());
+  });
 });
 
 function addTodos(todos) {
@@ -15,7 +24,9 @@ function addTodos(todos) {
 }
 
 function addTodo(todo) {
-  var newTodo = $('<li class="task">' + todo.name + '</li>');
+  var newTodo = $('<li class="task">' + todo.name + ' <span>X</span></li>');
+  newTodo.data('id', todo._id);
+  newTodo.data('completed', todo.completed);
   if (todo.completed) {
     newTodo.addClass('done');
   }
@@ -32,4 +43,33 @@ function createTodo() {
     .catch(err => {
       console.log(err);
     });
+}
+
+function removeTodo(todo) {
+  var clickedId = todo.data('id');
+  var deleteUrl = '/api/todos/' + clickedId;
+  $.ajax({
+    type: 'DELETE',
+    url: deleteUrl,
+  })
+    .then(function(data) {
+      todo.remove();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function updateTodo(todo) {
+  var updateUrl = '/api/todos/' + todo.data('id');
+  var isDone = !todo.data('completed');
+  var updateData = { completed: isDone };
+  $.ajax({
+    type: 'put',
+    url: updateUrl,
+    data: updateData,
+  }).then(function(updatedTodo) {
+    todo.toggleClass('done');
+    todo.data('completed', isDone);
+  });
 }
--- todos_api/config.js
+++ todos_api/config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/todo';
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || 'mongodb://localhost:8080/test-mobile-detail-app';
-exports.PORT = process.env.PORT || 8000;
+exports.PORT = process.env.PORT || 3000;
